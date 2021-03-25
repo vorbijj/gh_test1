b943749c99c492b884469e81c46cbc39fd7a3b44
Ilya Vorobyev <vorbinn@list.ru>
2021-03-26 00:40:03 +0300

    добавлен random

--- src/com/company/less1/Main.java
+++ src/com/company/less1/Main.java
@@ -1,8 +1,20 @@
 package com.company.less1;
 
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello, world!!!");
+
+        int min = -10;
+        int max = 10;
+        int diff = max - min;
+        Random random = new Random();
+        int i = random.nextInt(diff + 1);
+        i += min;
+
+        System.out.println(i);
+
     }
 }
